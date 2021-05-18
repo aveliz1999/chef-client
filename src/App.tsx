6d27f646a851252c5a51030fa8bc679e209d47de
aveliz1999 <aveliz1999@gmail.com>
2021-05-18 11:25:44 -0400

    Request temporary token on page load

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styles from './App.module.css';
 import Editor from "./components/editor/Editor";
+import axios from 'axios';
 
 function App() {
+    const [token, setToken] = useState("");
+
+    useEffect(() => {
+        axios.post('/api/auth')
+            .then(res => {
+                setToken(res.data);
+            });
+    }, []);
+
     return <div className={styles.container}>
         <div className={styles.topBar}>
-            <button>Run</button>
+            <button disabled={!token}>Run</button>
             <button>Sign In</button>
         </div>
         <Editor offset={50}/>
