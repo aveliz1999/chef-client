ec8b53251d7a0eecb842661018c3fb1ee07a6aee
aveliz1999 <aveliz1999@gmail.com>
2021-05-19 09:26:42 -0400

    Implement sending github code to server

--- src/components/githubLoginPage/GithubLoginPage.tsx
+++ src/components/githubLoginPage/GithubLoginPage.tsx
@@ -1,12 +1,31 @@
 import {useLocation} from 'react-router-dom';
 import styles from './GithubLoginPage.module.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function useQuery() {
     return new URLSearchParams(useLocation().search);
 }
 
 function GithubLoginPage() {
-    const code = useQuery().get('code');
+    const [code, setCode] = useState("");
+    const query = useQuery();
+
+    useEffect(() => {
+        setCode(query.get('code') || "");
+    }, []);
+
+    useEffect(() => {
+        if(!code) {
+            return;
+        }
+        axios.post('/api/auth/github', {
+            'code': code
+        }).then(res => {
+            // TODO handle the authentication response
+            console.log(res.data);
+        })
+    }, [code]);
 
     // TODO use the code to authenticate with the server
     return <div className={styles.container}>
